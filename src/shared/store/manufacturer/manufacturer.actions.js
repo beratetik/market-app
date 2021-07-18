09ef315413df3dcb8e61c8a38e1acf9e83001d5c
Berat Tetik <brt1905@gmail.com>
2021-07-18 18:15:03 +0300

    reverting env file updates

--- src/shared/store/manufacturer/manufacturer.actions.js
+++ src/shared/store/manufacturer/manufacturer.actions.js
@@ -2,11 +2,10 @@ import axios from 'axios'
 
 import * as manufacturerTypes from './manufacturer.types'
 
-const { REACT_APP_API_URL } = process.env
 // Action Creators
 export const getManufacturers = () => async (dispatch) => {
   dispatch(setLoading());
-  const response = await axios.get(`${REACT_APP_API_URL}/companies`)
+  const response = await axios.get('http://127.0.0.1:3001/companies')
 
   dispatch({
     type: manufacturerTypes.GET_MANUFACTURERS,
